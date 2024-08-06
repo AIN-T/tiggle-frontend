8a0db8ded9a6dac200052dbb5dd6400bdd66bc80
yunseul <siddl333@naver.com>
2024-08-06 18:10:55 +0900

    [Feat] 마이페이지 컴포넌트 및 페이지 구현

---
+++ src/pages/Mypage.vue
@@ -0,0 +1,22 @@
+<template>
+  <HeaderComponent />
+  <MyPageComponent />
+  <FooterComponent />
+</template>
+
+<script>
+import HeaderComponent from '../components/common/HeaderComponent.vue';
+import MyPageComponent from '../components/mypage/MyPageComponent.vue';
+import FooterComponent from '../components/common/FooterComponent.vue';
+
+export default {
+  name: 'MyPage',
+  components: {
+    HeaderComponent,
+    MyPageComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+<style scoped></style>
