e04c8f4623d374dfd68f6d161b09549ab6779555
yunseul <siddl333@naver.com>
2024-08-07 14:25:16 +0900

    [Feat] formatRefundDate,  formatRefundStringDate 함수 구현

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -11,22 +11,42 @@ const formatDate = (dateString) => {
 };
 
 const formatDateTime = (dateTime) => {
-  // Date 객체 생성
   const date = new Date(dateTime);
 
-  // 연도, 월, 일, 시간, 분 추출
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
   const day = String(date.getDate()).padStart(2, '0');
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
-  // 요일 배열
   const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
   const dayOfWeek = daysOfWeek[date.getDay()]; // 요일 추출
 
-  // 최종 형식으로 반환
   return `${year}.${month}.${day}(${dayOfWeek}) ${hours}:${minutes}`;
 };
 
-export { formatDate, formatDateTime };
+const formatRefundDate = (dateTime) => {
+  const date = new Date(dateTime);
+
+  date.setDate(date.getDate() - 7);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
+};
+
+const formatRefundStringDate = (dateTime) => {
+  const date = new Date(dateTime);
+
+  date.setDate(date.getDate() - 7);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+export { formatDate, formatDateTime, formatRefundDate, formatRefundStringDate };
