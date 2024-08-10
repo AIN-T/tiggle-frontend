d5d53b495a24e67c27b9519bbf98dc001a2cde8b
yunseul <siddl333@naver.com>
2024-08-10 18:53:34 +0900

    [Feat] 구역 정보 조회 stor 구현

--- src/stores/useBookingStore.js
+++ src/stores/useBookingStore.js
@@ -9,14 +9,18 @@ export const useBookingStore = defineStore('booking', {
       programId: '',
       timesId: '',
       sectionId: '',
+      locationId: '',
       seatLists: [],
+      price: '',
     },
     reservationId: '',
     reservation: {},
+    sections: {},
   }),
   persist: true,
   actions: {
-    setData(programName, programId, timesId, sectionId) {
+    setData(locationId, programName, programId, timesId, sectionId) {
+      this.book.locationId = locationId;
       this.book.programName = programName;
       this.book.programId = programId;
       this.book.timesId = timesId;
@@ -38,7 +42,7 @@ export const useBookingStore = defineStore('booking', {
       return this.book.seatLists;
     },
 
-    async setSeat(programId, timesId, seatId) {
+    async setSeat(programId, timesId, seatId, price) {
       const res = await axios.post(
         '/api/verify',
         { programId: programId, timesId: timesId, seatId: seatId },
@@ -47,6 +51,7 @@ export const useBookingStore = defineStore('booking', {
 
       if (res.status == 200) {
         this.reservationId = res.data.result.reservationId;
+        this.book.price = price;
 
         router.push('/price');
       }
@@ -65,6 +70,28 @@ export const useBookingStore = defineStore('booking', {
       } catch (error) {
         console.log('Failed get', error);
       }
+    },
+
+    async getSection() {
+      try {
+        const response = await axios.get(
+          '/api/section?locationId=' +
+            this.book.locationId +
+            '&programId=' +
+            this.book.programId,
+          {
+            withCredentials: true,
+          }
+        );
+
+        this.sections = response.data.result;
+      } catch (error) {
+        console.log('Failed get', error);
+      }
+    },
+
+    setSection(id) {
+      this.book.sectionId = id;
     },
   },
 });
